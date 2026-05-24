4a0d60a24b7181707f3e63d74aee58206ccca19c
mohamedredjam-red <mohamedredjam67@gmail.com>
2026-05-24 15:08:43 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -50,3 +50,59 @@ $(document).ready(function(){
         })
     })
 })
+
+
+
+let enddate = "2026-06-20T00:23:00";
+
+document.querySelector("#head").innerText =
+"20 june 2026 12:33 AM";
+
+let inputs = document.querySelectorAll(".timer-banner input");
+
+function clock() {
+
+    let end = new Date(enddate).getTime();
+
+    let now = new Date().getTime();
+
+    let diff = end - now;
+
+    if (diff <= 0) {
+
+        inputs[0].value = "00";
+        inputs[1].value = "00";
+        inputs[2].value = "00";
+        inputs[3].value = "00";
+
+        return;
+    }
+
+    let days = Math.floor(
+        diff / (1000 * 60 * 60 * 24)
+    );
+
+    let hours = Math.floor(
+        (diff % (1000 * 60 * 60 * 24))
+        / (1000 * 60 * 60)
+    );
+
+    let minutes = Math.floor(
+        (diff % (1000 * 60 * 60))
+        / (1000 * 60)
+    );
+
+    let seconds = Math.floor(
+        (diff % (1000 * 60))
+        / 1000
+    );
+
+    inputs[0].value = days;
+    inputs[1].value = hours;
+    inputs[2].value = minutes;
+    inputs[3].value = seconds;
+}
+
+clock();
+
+setInterval(clock, 1000);
